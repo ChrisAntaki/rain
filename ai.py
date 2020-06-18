fbf16c1949f286803ea9f36afe39355cccae55a6
Chris Antaki <ChrisAntaki@gmail.com>
2020-06-17 20:59:34 -0700

    TF2 conversion

--- ai.py
+++ ai.py
@@ -18,51 +18,51 @@ from glob import glob
 import json
 import sys
 import subprocess
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
-tf.disable_v2_behavior()
+tf.compat.v1.disable_eager_execution()
 
 size_input = 5
 size_hidden = 200
 size_output = 3
 
-a = tf.placeholder(tf.float32)
-t = tf.placeholder(tf.float32, [None, size_output])
-x = tf.placeholder(tf.float32, [None, size_input])
-keep_prob = tf.placeholder(tf.float32)
+a = tf.compat.v1.placeholder(tf.float32)
+t = tf.compat.v1.placeholder(tf.float32, [None, size_output])
+x = tf.compat.v1.placeholder(tf.float32, [None, size_input])
+keep_prob = tf.compat.v1.placeholder(tf.float32)
 
 class AI(object):
   '''AI for the rain game's player.'''
   def __init__(self, restore=True, save=True):
     # Define variables.
-    weights_1 = tf.Variable(tf.truncated_normal([size_input, size_hidden]))
-    weights_2 = tf.Variable(tf.truncated_normal([size_hidden, size_output]))
+    weights_1 = tf.Variable(tf.random.truncated_normal([size_input, size_hidden]))
+    weights_2 = tf.Variable(tf.random.truncated_normal([size_hidden, size_output]))
     biases_1 = tf.Variable(tf.zeros([size_hidden]))
     biases_2 = tf.Variable(tf.zeros([size_output]))
 
     # Define model.
     model = tf.matmul(x, weights_1) + biases_1
     model = tf.nn.relu(model)
-    model = tf.nn.dropout(model, keep_prob)
+    model = tf.nn.dropout(model, 1 - (keep_prob))
     model = tf.matmul(model, weights_2) + biases_2
-    self.prediction = tf.argmax(model, 1)
-    self.loss = a * tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
+    self.prediction = tf.argmax(input=model, axis=1)
+    self.loss = a * tf.reduce_mean(input_tensor=tf.nn.softmax_cross_entropy_with_logits(
         labels=t, logits=model))
     self.average_score = 0.0
-    self.train = tf.train.AdamOptimizer(1e-4).minimize(self.loss)
-    self.sess = tf.Session()
+    self.train = tf.compat.v1.train.AdamOptimizer(1e-4).minimize(self.loss)
+    self.sess = tf.compat.v1.Session()
     if save:
-      self.saver = tf.train.Saver()
+      self.saver = tf.compat.v1.train.Saver()
     self.checkpoint_path = '/tmp/rain.ckpt'
 
     # Add a pinch of chaos.
-    min_prob = tf.reduce_min(model, 1)
+    min_prob = tf.reduce_min(input_tensor=model, axis=1)
     boosted_y = min_prob + model
-    shuffled_y = boosted_y * tf.random_uniform([3])
-    self.shuffled_prediction = tf.argmax(shuffled_y, 1)
+    shuffled_y = boosted_y * tf.random.uniform([3])
+    self.shuffled_prediction = tf.argmax(input=shuffled_y, axis=1)
 
     # Initialize/reset global variables.
-    self.sess.run(tf.global_variables_initializer())
+    self.sess.run(tf.compat.v1.global_variables_initializer())
     if restore:
       try:
         self.saver.restore(self.sess, self.checkpoint_path)
